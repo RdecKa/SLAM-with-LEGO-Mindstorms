e4d7fa2391a38586bda4691336795fef75735b36
Karin Frlic <kf5121@student.uni-lj.si>
2020-03-25 21:15:43 +0100

    Draw path

--- slam/agent/robot.py
+++ slam/agent/robot.py
@@ -12,7 +12,7 @@ class Robot(agent.Agent):
     def __init__(self, data_queue: queue.Queue):
         super().__init__(data_queue)
         self.map = dict()
-        self.pose = geometry.Pose(10, 10, 0)
+        self.pose = geometry.Pose(0, 0, 0)
 
     def move_forward(self, distance: float):
         logging.info(f"Move formward for {distance}.")
@@ -27,20 +27,14 @@ class Robot(agent.Agent):
 
     def perform_action(self):
         r = random.random()
-        if r < 0.5:
-            self.move_forward(1)
-        elif r < 0.6:
-            self.rotate(90)
-        elif r < 0.7:
-            self.rotate(-90)
-        elif r < 0.8:
-            self.rotate(180)
-        elif r < 0.9:
-            self.rotate(270)
+        if r < 0.7:
+            distance = random.randint(1, 5)
+            self.move_forward(distance)
         else:
-            self.move_forward(3)
+            angle = random.randint(1, 360)
+            self.rotate(angle)
 
         data = datapoint.Position(*self.pose.get_coordinates())
         self.data_queue.put(data)
 
-        time.sleep(3)
+        time.sleep(1)
--- slam/display/map.py
+++ slam/display/map.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.patches as patches
+import matplotlib.path as mpath
 
 plt.ion()
 
 
 class Map():
-    def __init__(self):
+    def __init__(self, draw_path: bool = True):
         self.data = np.empty([0, 2])
         self.cdata = np.empty([0, 4])
+        self.draw_path = draw_path
         self.init_graph()
 
     def init_graph(self):
@@ -19,6 +22,12 @@ class Map():
         self.scat.remove()
         self.scat = self.ax.scatter(self.data[:, 0], self.data[:, 1],
                                     c=self.cdata)
+
+        if self.draw_path:
+            path = mpath.Path(self.data, self.compute_path_codes())
+            patch = patches.PathPatch(path, fill=False, lw=1)
+            self.ax.add_patch(patch)
+
         # Draw and flush
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
@@ -26,3 +35,8 @@ class Map():
     def add_data(self, data):
         self.data = np.vstack((self.data, (data.x, data.y)))
         self.cdata = np.vstack((self.cdata, data.color))
+
+    def compute_path_codes(self):
+        if (len(self.data) == 0):
+            return []
+        return [mpath.Path.MOVETO] + [mpath.Path.LINETO] * (len(self.data) - 1)
