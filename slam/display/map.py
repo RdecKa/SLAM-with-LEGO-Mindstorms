620514a0f18af844d4cbc01d89068f80fc411da3
Karin Frlic <kf5121@student.uni-lj.si>
2020-04-07 12:22:19 +0200

    Show robot size on the map, refactor map

--- slam/display/map.py
+++ slam/display/map.py
@@ -1,7 +1,7 @@
+import matplotlib.collections as collections
 import matplotlib.patches as patches
 import matplotlib.path as mpath
 import matplotlib.pyplot as plt
-import numpy as np
 
 import slam.common.datapoint as datapoint
 import slam.display.storage as storage
@@ -11,9 +11,10 @@ plt.ion()
 
 
 class Map():
-    def __init__(self, draw_path: bool = True):
+    def __init__(self, robot_size: float = 10.0, draw_path: bool = True):
         self.storage = storage.MapStorage(draw_path)
         self.draw_path = draw_path
+        self.robot_size = robot_size
         self.init_graph()
 
     def init_graph(self):
@@ -30,25 +31,11 @@ class Map():
                 self.heat[3].remove()
                 del self.heat
 
-            x_max, x_min = max(x_heat), min(x_heat)
-            y_max, y_min = max(y_heat), min(y_heat)
-            x_diff = x_max - x_min
-            y_diff = y_max - y_min
-            if x_diff > y_diff:
-                x_margin = x_diff * 0.1
-                total_size = x_diff + 2 * x_margin
-                y_margin = (total_size - y_diff) / 2
-            else:
-                y_margin = y_diff * 0.1
-                total_size = y_diff + 2 * y_margin
-                x_margin = (total_size - x_diff) / 2
-            map_range = [[x_min - x_margin, x_max + x_margin],
-                         [y_min - y_margin, y_max + y_margin]]
+            map_range, bins = self.calculate_params_for_heatmap(x_heat, y_heat)
 
-            bins = int(min(max(1, x_diff, y_diff), 60))
             self.heat = self.ax.hist2d(x_heat, y_heat, weights=w_heat,
                                        bins=bins, range=map_range, cmap="BrBG",
-                                       alpha=.3, vmin=-10, vmax=10)
+                                       alpha=0.3, vmin=-10, vmax=10)
 
         # Scatter plot
         if self.scat:
@@ -58,22 +45,58 @@ class Map():
         x_scat, y_scat, c_scat = self.storage.get_scatter_data()
         self.scat = self.ax.scatter(x_scat, y_scat, c=c_scat)
 
-        # Path
+        # Path and circles
         if self.draw_path:
             path_data = self.storage.get_path_data()
             for path_id in path_data:
-                if path_id in self.path and self.path[path_id]:
-                    self.path[path_id].remove()
+                if path_id in self.path:
+                    for p in self.path[path_id]:
+                        if p:
+                            p.remove()
                 path_data_entry = path_data[path_id]
-                codes = self.compute_path_codes(path_data_entry.data)
-                path = mpath.Path(path_data_entry.data, codes)
-                patch = patches.PathPatch(path, fill=False, lw=1,
-                                          ls=path_data_entry.linestyle)
-                self.path[path_id] = self.ax.add_patch(patch)
+                self.path[path_id] = self.create_patches(path_data_entry)
 
         # Draw and flush
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
+
+    def calculate_params_for_heatmap(self, x_data, y_data):
+        x_max, x_min = max(x_data), min(x_data)
+        y_max, y_min = max(y_data), min(y_data)
+        x_diff = x_max - x_min
+        y_diff = y_max - y_min
+
+        if x_diff > y_diff:
+            x_margin = x_diff * 0.1
+            total_size = x_diff + 2 * x_margin
+            y_margin = (total_size - y_diff) / 2
+        else:
+            y_margin = y_diff * 0.1
+            total_size = y_diff + 2 * y_margin
+            x_margin = (total_size - x_diff) / 2
+
+        map_range = [[x_min - x_margin, x_max + x_margin],
+                     [y_min - y_margin, y_max + y_margin]]
+        bins = int(min(max(1, x_diff, y_diff), 60))
+        return map_range, bins
+
+    def create_patches(self, path_data_entry: storage.PathStorageEntry):
+        codes = self.compute_path_codes(path_data_entry.data)
+        path = mpath.Path(path_data_entry.data, codes)
+        path_patch = patches.PathPatch(path, fill=False, lw=1,
+                                       ls=path_data_entry.linestyle)
+        circle_patches = []
+        for row in path_data_entry.data:
+            circle = patches.Circle(row, radius=self.robot_size / 2)
+            circle_patches.append(circle)
+
+        collection = collections.PatchCollection(circle_patches, alpha=0.3,
+                                                 color=(0.7, 0.7, 0.7))
+
+        return [
+            self.ax.add_patch(path_patch),
+            self.ax.add_collection(collection),
+        ]
 
     def add_data(self, data: datapoint.DataPoint):
         if data.graph_type == GraphType.SCATTER:
--- slam/driver.py
+++ slam/driver.py
@@ -11,10 +11,17 @@ def run():
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
-    map = smap.Map()
+    robot_size = 20.0
+    scanning_precision = 20
+    view_angle = 240
+    world_number = 5
+
+    map = smap.Map(robot_size=robot_size)
     data_queue = queue.Queue()
-    agent = robot.LegoRobot(data_queue, scanning_precision=10, view_angle=240,
-                            robot_size=25.0)
+    agent = robot.SimulatedRobot(data_queue,
+                                 scanning_precision=scanning_precision,
+                                 view_angle=view_angle, robot_size=robot_size,
+                                 world_number=world_number)
     agent.start()
 
     try:
